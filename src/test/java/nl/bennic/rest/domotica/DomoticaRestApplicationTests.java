5af15f0d26120c33ee8ed2c2f9a4797957425580
Bennic1 <Ben@nicola.nl>
2020-08-18 11:49:54 +0200

    Tests

--- src/test/java/nl/bennic/rest/domotica/DomoticaRestApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package nl.bennic.rest.domotica;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DomoticaRestApplicationTests {
-
-}
